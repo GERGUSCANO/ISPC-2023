faacbf0c45d5a0e33e6f82bc4919c2dd446ff156
german <germangustavocano@gmail.com>
2023-05-03 10:54:21 -0300

    #TK009

--- FRONT/js/script.js
+++ FRONT/js/script.js
@@ -2,3 +2,9 @@ const ProductosFarmacia = {
     
     
 }
+fetch('vademecum.json', {
+    mode: 'cors',
+    credentials: 'include'
+  })
+    .then(response => response.json())
+    .then (data =>console.log(data));
