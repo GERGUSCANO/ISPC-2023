f1eb920cc5dd8d723145efbe916ae7b74b848a9a
Sarja97 <ivansarja1997@gmail.com>
2023-06-04 16:48:40 -0300

    Se agrego validacion de formularios reactivos y clases de control de estado a login-form

--- FRONT/src/app/pages/form-login/form-login.component.ts
+++ FRONT/src/app/pages/form-login/form-login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-login',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-login.component.css']
 })
 export class FormLoginComponent {
+    loginForm = this.fB.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+
+
+
+   constructor(private fB: FormBuilder, private router: Router){
+
+   }
+
+   get email(){
+    return this.loginForm.controls.email;
+   }
+   get password(){
+    return this.loginForm.controls.password;
+   }
+
+
+
+
+   login(){
+    if(this.loginForm.valid){
+      console.log("llamar al servicio de logueo");
+      this.router.navigateByUrl('/products')
+      this.loginForm.reset();
+    }else{
+      this.loginForm.markAllAsTouched();
+
+    }
+   }
+
+
 
 }
