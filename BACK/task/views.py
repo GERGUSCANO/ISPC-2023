1911cbbd51c0186c0840086bb09adaa12f21ee5a
german <germangustavocano@gmail.com>
2023-06-13 19:33:22 -0300

    .

--- BACK/task/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
-
-def form_registro(request):
-    return render(request, '.html', {
-        'form': UserCreationForm
-    })
